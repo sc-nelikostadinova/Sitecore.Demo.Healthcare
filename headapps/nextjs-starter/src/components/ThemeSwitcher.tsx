f4a493536b30bcaa32a2942000d5812038e73a2c
Neli Kostadinova <110887014+sc-nelikostadinova@users.noreply.github.com>
2025-06-26 12:46:35 +0300

    Theme Switcher component (#2)
    * Theme Switcher component

---
+++ headapps/nextjs-starter/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+export const Default = (): JSX.Element => {
+  const [isDark, setIsDark] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const storedTheme = sessionStorage.getItem('theme');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setIsDark(storedTheme ? storedTheme === 'dark' : prefersDark);
+  }, []);
+
+  useEffect(() => {
+    if (isDark === null) return;
+
+    document.body.classList.toggle('dark', isDark);
+    sessionStorage.setItem('theme', isDark ? 'dark' : 'light');
+  }, [isDark]);
+
+  return (
+    <div className="inline-block">
+      <button
+        aria-label={`Switch to ${isDark ? 'light' : 'dark'} mode`}
+        onClick={() => setIsDark(!isDark)}
+        className={`
+          w-13 h-7 p-0.5 rounded-full text-left
+          bg-foreground dark:bg-foreground-dark
+          transition-colors duration-300
+        `}
+      >
+        <span
+          className={`
+            inline-block w-6 h-6 rounded-full
+            bg-background dark:bg-background-dark dark:translate-x-6
+            transition-all duration-300
+          `}
+        />
+      </button>
+    </div>
+  );
+};
