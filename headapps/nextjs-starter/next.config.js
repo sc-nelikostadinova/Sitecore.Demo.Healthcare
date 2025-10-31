e55019b7e98d1b8ff5112453e1e63a4b61b26095
Brandon Bruno <brandon.bruno@sitecore.com>
2025-10-31 09:43:52 -0400

    Updated next.config with template version.

--- headapps/nextjs-starter/next.config.js
+++ headapps/nextjs-starter/next.config.js
@@ -5,7 +5,6 @@ const SassAlias = require('sass-alias');
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
 
@@ -14,7 +13,7 @@ const nextConfig = {
     // These should generally match (or at least be a subset of) those in Sitecore.
     locales: ['en'],
     // This is the locale that will be used when visiting a non-locale
-    // prefixed path e.g. `/styleguide`.
+    // prefixed path e.g. `/about`.
     defaultLocale: process.env.DEFAULT_LANGUAGE || process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE || 'en',
   },
 
@@ -28,7 +27,6 @@ const nextConfig = {
   // can be served from the Next.js Image Optimization API
   // see https://nextjs.org/docs/app/api-reference/components/image#remotepatterns
   images: {
-    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -40,12 +38,7 @@ const nextConfig = {
         hostname: 'xmc-*.**',
         port: '',
       },
-      {
-        protocol: 'https',
-        hostname: 'feaas*.blob.core.windows.net',
-        port: '',
-      },
-    ]
+    ],
   },
 
   async rewrites() {
@@ -73,162 +66,40 @@ const nextConfig = {
     ];
   },
 
-  componentPropsPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-      webpack: (config, options) => {
-        
-        if (!options.isServer) {
-          // Add a loader to strip out getServerSideProps and getStaticProps from components in the client bundle
-          config.module.rules.unshift({
-            test: /src\\components\\.*\.tsx$/,
-            use: ['@sitecore-jss\\sitecore-jss-dev-tools\\nextjs-component-props-loader'],
-          });
-        }
-
-        // Overload the Webpack config if it was already overloaded
-        if (typeof nextConfig.webpack === 'function') {
-          return nextConfig.webpack(config, options);
-        }
-
-        return config;
-      },
-    });
-  },
-
-  corsHeaderPlugin(nextConfig = {}) {
-    if (!config.sitecoreApiHost) {
-      return nextConfig;
-    }
-    return Object.assign({}, nextConfig, {
-      async headers() {
-        const extendHeaders =
-          typeof nextConfig.headers === 'function' ? await nextConfig.headers() : [];
-        return [
-          ...(await extendHeaders),
-          {
-            source: '/_next/:path*',
-            headers: [
-              {
-                key: 'Access-Control-Allow-Origin',
-                value: config.sitecoreApiHost.replace(/\/$/, ''),
-              },
-            ],
-          },
-        ];
-      },
-    });
-  },
-
-  feaasPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-      async rewrites() {
-        return [
-          ...await nextConfig.rewrites(),
-          {
-            source: '/feaas-render',
-            destination: '/api/editing/feaas/render',
-          },
-        ];
-      },
-      webpack: (config, options) => {
-        if (options.isServer) {
-          // Force use of CommonJS on the server for FEAAS SDK since JSS also uses CommonJS entrypoint to FEAAS SDK.
-          // This prevents issues arising due to FEAAS SDK's dual CommonJS/ES module support on the server (via conditional exports).
-          // See https://nodejs.org/api/packages.html#dual-package-hazard.
-          config.externals = [
-            {
-              '@sitecore-feaas/clientside/react': 'commonjs @sitecore-feaas/clientside/react',
-              '@sitecore/byoc': 'commonjs @sitecore/byoc',
-              '@sitecore/byoc/react': 'commonjs @sitecore/byoc/react',
-            },
-            ...config.externals,
-          ];
-        }
-
-        // Overload the Webpack config if it was already overloaded
-        if (typeof nextConfig.webpack === 'function') {
-          return nextConfig.webpack(config, options);
-        }
-
-        return config;
-      },
-    });
-  },
-
-  graphqlPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-      webpack: (config, options) => {
-        config.module.rules.push({
-          test: /\.graphql$/,
-          exclude: /node_modules/,
-          use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
-        });
-      
-        config.module.rules.push({
-          test: /\.graphqls$/,
-          exclude: /node_modules/,
-          use: ['graphql-let/schema/loader'],
-        });
-      
-        config.module.rules.push({
-          test: /\.ya?ml$/,
-          type: 'json',
-          use: 'yaml-loader',
-        });
-
-        // Overload the Webpack config if it was already overloaded
-        if (typeof nextConfig.webpack === 'function') {
-          return nextConfig.webpack(config, options);
-        }
-
-        return config;
-      }
-    });
-  },
-
-  sassPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-        sassOptions: {
-          importer: new SassAlias({
-            '@sass': path.join(__dirname, '../../../assets', 'sass'),
-            '@fontawesome': path.join(__dirname, '../../../../node_modules', 'font-awesome'),
-          }).getImporter(),
-          quietDeps: true,
-          silenceDeprecations: ["import", "legacy-js-api"],
-        },
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      // Add a loader to strip out getComponentServerProps from components in the client bundle
+      config.module.rules.unshift({
+        test: /src\\components\\.*\.tsx$/,
+        use: ['@sitecore-content-sdk\\nextjs\\component-props-loader'],
       });
+    } else {
+      // Force use of CommonJS on the server for FEAAS SDK since Content SDK also uses CommonJS entrypoint to FEAAS SDK.
+      // This prevents issues arising due to FEAAS SDK's dual CommonJS/ES module support on the server (via conditional exports).
+      // See https://nodejs.org/api/packages.html#dual-package-hazard.
+      config.externals = [
+        {
+          '@sitecore-feaas/clientside/react': 'commonjs @sitecore-feaas/clientside/react',
+          '@sitecore/byoc': 'commonjs @sitecore/byoc',
+          '@sitecore/byoc/react': 'commonjs @sitecore/byoc/react',
+        },
+        ...config.externals,
+      ];
+    }
+
+    return config;
   },
 
-  robotsPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-      async rewrites() {
-        return [
-          ...await nextConfig.rewrites(),
-          // robots route
-          {
-            source: '/robots.txt',
-            destination: '/api/robots',
-          },
-        ];
-      },
-    });
+  // Add sass settings for SXA themes and styles
+  sassOptions: {
+    importer: new SassAlias({
+      '@globals': path.join(process.cwd(), './src/assets', 'globals'),
+      '@fontawesome': path.join(process.cwd(), './node_modules', 'font-awesome'),
+    }).getImporter(),
+    // temporary measure until new versions of bootstrap and font-awesome released
+    quietDeps: true,    
+    silenceDeprecations: ["import", "legacy-js-api"],
   },
-
-  sitemapPlugin(nextConfig = {}) {
-    return Object.assign({}, nextConfig, {
-      async rewrites() {
-        return [
-          ...await nextConfig.rewrites(),
-          // sitemap route
-          {
-            source: '/sitemap:id([\\w-]{0,}).xml',
-            destination: '/api/sitemap'
-          },
-        ];
-      },
-    });
-  }
-
 };
 
 module.exports = nextConfig;
